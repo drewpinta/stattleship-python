63ea401b22510bc0b57ce6461bc81d801fee17ce
Drew Pinta <dpinta6@gmail.com>
2016-05-08 15:55:47 -0400

    Add instructions for installing from github with pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='stattlepy',
-    version='0.1dev',
+    version='0.0.1',
     author = 'Adam Jenkins',
     packages=['stattlepy'],
     license='MIT',
